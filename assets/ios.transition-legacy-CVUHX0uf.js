715e84f115d6ba400918b318bd91e35aa4de9a17
ParkalotDev3 <pawel@parkalot.io>
2024-09-29 06:39:39 +0200

    Updates

--- assets/ios.transition-legacy-CVUHX0uf.js
+++ assets/ios.transition-legacy-CVUHX0uf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D6_CiGec.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bk16im1y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
